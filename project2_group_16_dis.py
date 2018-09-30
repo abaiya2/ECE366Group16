2e4c611bf43efadeada75f5019c446a9c82b1959
Amaan Baiyat <amaan_baiyat@live.com>
2018-09-29 19:31:58 -0500

    Corrected Input and Output files

--- project2_group_16_dis.py
+++ project2_group_16_dis.py
@@ -3,16 +3,18 @@
 # Arsalan Babar
 # This file disassembles two project files
 
-p1_input_file = open("project2_group_16_p1_asm.txt", "r")
-p1_output_file = open("project2_group_16_p1_bin.txt", "w")
+p1_input_file = open("project2_group_16_p1_bin.txt", "r")
+p1_output_file = open("project2_group_16_p1_asm.txt", "w")
 
-p2_input_file = open("project2_group_16_p2_asm.txt", "r")
-p2_output_file = open("project2_group_16_p2_bin.txt", "w")
+p2_input_file = open("project2_group_16_p2_bin.txt", "r")
+p2_output_file = open("project2_group_16_p2_asm.txt", "w")
 
 
 def convert_asm_to_bin(input_file, output_file):
     for line in input_file:
-        output_file.write(line)
+        if (line == "0000"):
+            output_file.write("ADD")
+
 
 
 convert_asm_to_bin(p1_input_file, p1_output_file)
