6f9924df153169fdb9dc56198b1237a83bc39c34
Amaan Baiyat <amaan_baiyat@live.com>
2018-09-29 19:20:33 -0500

    Added Skeleton function convert_asm_to_bin

--- project2_group_16_dis.py
+++ project2_group_16_dis.py
@@ -4,11 +4,19 @@
 # This file disassembles two project files
 
 p1_input_file = open("project2_group_16_p1_asm.txt", "r")
-p1_output_file = open("project2_group_16_p1_asm.txt", "w")
+p1_output_file = open("project2_group_16_p1_bin.txt", "w")
 
 p2_input_file = open("project2_group_16_p2_asm.txt", "r")
-p2_output_file = open("project2_group_16_p2_asm.txt", "w")
+p2_output_file = open("project2_group_16_p2_bin.txt", "w")
 
-p1_output_file.write("Project 1")
-p2_output_file.write("Project 2")
+
+def convert_asm_to_bin(input_file, output_file):
+    for line in input_file:
+        output_file.write(line)
+
+
+convert_asm_to_bin(p1_input_file, p1_output_file)
+convert_asm_to_bin(p2_input_file, p2_output_file)
+
+
 
