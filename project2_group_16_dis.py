1153fd7791720c3078cfc39e2299a6a291e246c4
Amaan Baiyat <amaan_baiyat@live.com>
2018-09-29 19:14:02 -0500

    Added File I/O

---
+++ project2_group_16_dis.py
@@ -0,0 +1,14 @@
+# ECE 366 Project 2 Disassembler
+# Amaan Baiyat
+# Arsalan Babar
+# This file disassembles two project files
+
+p1_input_file = open("project2_group_16_p1_asm.txt", "r")
+p1_output_file = open("project2_group_16_p1_asm.txt", "w")
+
+p2_input_file = open("project2_group_16_p2_asm.txt", "r")
+p2_output_file = open("project2_group_16_p2_asm.txt", "w")
+
+p1_output_file.write("Project 1")
+p2_output_file.write("Project 2")
+
