418033b119ed4211f11331c6f4ba8eb4974af454
Amaan Baiyat <amaan_baiyat@live.com>
2018-09-30 11:25:32 -0500

    Changed Convert method name

--- project2_group_16_dis.py
+++ project2_group_16_dis.py
@@ -10,15 +10,11 @@ p2_input_file = open("project2_group_16_p2_bin.txt", "r")
 p2_output_file = open("project2_group_16_p2_asm.txt", "w")
 
 
-def convert_asm_to_bin(input_file, output_file):
+def convert_bin_to_asm(input_file, output_file):
     for line in input_file:
         if (line == "0000"):
             output_file.write("ADD")
 
 
-
-convert_asm_to_bin(p1_input_file, p1_output_file)
-convert_asm_to_bin(p2_input_file, p2_output_file)
-
-
-
+convert_bin_to_asm(p1_input_file, p1_output_file)
+convert_bin_to_asm(p2_input_file, p2_output_file)
